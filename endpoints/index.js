41b5c3107033c587be26ae3f5f1d9a64f9b167db
Baptiste <baptiste@matierenoire.io>
2017-08-04 10:13:51 +0200

    add Grommet dep + remove mongoDB ref

--- endpoints/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import config from '../config';
-
-export const fetchUser = userId => {
-  return axios
-    .get(`${config.server}/api/user/${userId}`)
-    .then(res => res.data)
-    .catch(err => err.response.status);
-};
